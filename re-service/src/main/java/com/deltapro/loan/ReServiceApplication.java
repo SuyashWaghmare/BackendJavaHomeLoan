943ddd7285987afc99b967466ff2071320499b7f
SuyashWaghmare <suyashkumarwaghmare@gmail.com>
2023-07-31 13:32:32 +0530

    Project Created Master c1

---
+++ re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deltapro.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReServiceApplication.class, args);
+	}
+
+}
