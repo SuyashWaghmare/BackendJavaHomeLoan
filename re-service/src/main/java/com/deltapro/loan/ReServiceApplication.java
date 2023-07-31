52e98973cd985798ff711b5cd7d191e63a691903
sagar <vsagarpujar7.com>
2023-07-31 16:48:42 +0530

    vsagar changes c1

--- re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
+++ re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
@@ -12,7 +12,11 @@ public class ReServiceApplication {
 		System.out.println("Monika Changes v2");
 		System.out.println("Monika Changes v3");
 		System.out.println("Suyash Changes v3");
-		System.out.println("sagar Changes v4");
+		
+		
+		
+		
+		System.out.println("Monika Changes v7");
 		
 	}
 
