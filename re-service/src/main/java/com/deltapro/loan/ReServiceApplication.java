9a37c8c5a14de113e12fb24a812dd44527027712
Monika908 <patil.monika2004@gmail.com>
2023-07-31 13:48:16 +0530

    Monika changes c2

--- re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
+++ re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
@@ -9,6 +9,8 @@ public class ReServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReServiceApplication.class, args);
 		System.out.println("Monika Changes v1");
+		System.out.println("Monika Changes v2");
+		
 	}
 
 }
