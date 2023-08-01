8a25578b4739f8ca73cceb7e5f67c4447b6a920a
PradipPatil <pradippatil8063@gmail.com>
2023-08-01 11:06:32 +0530

    add One lone v10

--- re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
+++ re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
@@ -16,7 +16,7 @@ public class ReServiceApplication {
 		
 		
 		
-		
+		System.out.println("Pradip P...v10!!");
 		System.out.println("Sagar Changes v9");
 		System.out.println("sagar changes v10");
 		
