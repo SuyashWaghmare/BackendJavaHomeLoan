7646a3c152f6358e8e14f71bf3aeddc2e16c0bcf
SuyashWaghmare <suyashkumarwaghmare@gmail.com>
2023-08-01 13:52:01 +0530

    m2

--- re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
+++ re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
@@ -8,39 +8,6 @@ public class ReServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReServiceApplication.class, args);
-		System.out.println("Monika Changes v1");
-		System.out.println("Monika Changes v2");
-		System.out.println("Monika Changes v3");
-		System.out.println("Suyash Changes v3");
-		System.out.println("Rashmi Changes v4");
-		System.out.println("Monika Changes v5");
-
-		System.out.println("smita changes v7");
-		System.out.println("smita changes v7");
-		System.out.println("smita changes v7");
-		System.out.println("smita changes v7");
-		
-		
-		
-		
-		
-		
-		System.out.println("Suyash");
-		System.out.println("Suyash");
-		System.out.println("Suyash");
-		
-		
-		System.out.println("Sagar Changes v9");
-		System.out.println("sagar changes v10");
-		
-		
-		System.out.println("Rashmi Changes v11");
-		
-		
-		System.out.println("Sagar changes v12");
-
-		System.out.println("Sagar changes v13");
-		
 	}
 
 }
