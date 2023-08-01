dc025632829f5c925e4e471101a7b0e581e4b6b9
RahulNagawade12 <nagawaderahul0@gmail.com>
2023-08-01 13:30:22 +0530

    Rahul changes c2

--- re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
+++ re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
@@ -32,6 +32,10 @@ public class ReServiceApplication {
 		System.out.println("Sagar Changes v9");
 		System.out.println("sagar changes v10");
 		
+		
+		System.out.println("Rahul Changes v11");
+		System.out.println("Rahul Changes v12");
+		
 	}
 
 }
