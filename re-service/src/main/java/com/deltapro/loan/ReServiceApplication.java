1a8eb48eddf915f5c237a37c084497164baf5e36
Smitamahajan911 <smitamahajan911@gmail.com>
2023-08-04 13:03:16 +0530

    c3

--- re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
+++ re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
@@ -8,14 +8,8 @@ public class ReServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReServiceApplication.class, args);
-		
-		
-		
-		
-		
-		
-		
-		
+
+		System.out.println("Deltapro loan app...");
 	}
 
 }
