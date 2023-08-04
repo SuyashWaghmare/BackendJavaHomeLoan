b3bb7751487646b61a0951ff34447638653c17f7
Smitamahajan911 <smitamahajan911@gmail.com>
2023-08-04 10:32:14 +0530

    smita c2

--- re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
+++ re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
@@ -8,18 +8,6 @@ public class ReServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReServiceApplication.class, args);
-		System.out.println("Monika Changes v1");
-		System.out.println("Monika Changes v2");
-		System.out.println("Monika Changes v3");
-		System.out.println("Suyash Changes v3");
-		System.out.println("Monika Changes v5");
-
-		System.out.println("smita changes v7");
-		System.out.println("smita changes v7");
-		System.out.println("smita changes v7");
-		System.out.println("smita changes v7");
-		
-		System.out.println("SMITA");
 		
 		
 		
@@ -27,10 +15,6 @@ public class ReServiceApplication {
 		
 		
 		
-		
-		
-		System.out.println("Sagar Changes v9");
-		System.out.println("sagar changes v10");
 		
 	}
 
