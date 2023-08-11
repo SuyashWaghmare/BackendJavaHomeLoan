72d346db0f8f201d213e4f6ac088fc990310ec3b
SuyashWaghmare <suyashkumarwaghmare@gmail.com>
2023-08-12 03:46:09 +0530

    some unnecessary files removed

--- re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
+++ re-service/src/main/java/com/deltapro/loan/ReServiceApplication.java
@@ -1,5 +1,6 @@
 package com.deltapro.loan;
 
+
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
