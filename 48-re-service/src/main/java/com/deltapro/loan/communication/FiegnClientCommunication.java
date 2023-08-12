192090678664877759d232b5401deac689d2b527
SuyashWaghmare <suyashkumarwaghmare@gmail.com>
2023-08-12 15:11:25 +0530

    Master added Files to RE

--- 48-re-service/src/main/java/com/deltapro/loan/communication/FiegnClientCommunication.java
+++
@@ -1,8 +0,0 @@
-package com.deltapro.loan.communication;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(url = "http://localhost:yourPortNo/", name = "YOUR-SERVICE-NAME")
-public interface FiegnClientCommunication {
-
-}
--- 48-re-service/src/main/java/com/deltapro/loan/repository/CustomerEnquiryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.deltapro.loan.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.deltapro.loan.model.CustomerEnquiry;
-
-public interface CustomerEnquiryRepository extends JpaRepository<CustomerEnquiry, Integer> {
-
-}
